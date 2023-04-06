23f52494ea2b38481d1f72fbaf2c6e4af836df1f
JedMoutahir <77209356+JedMoutahir@users.noreply.github.com>
2023-04-06 22:41:54 +0200

    Add files via upload

--- webscraping/main.py
+++ webscraping/main.py
@@ -1,33 +1,47 @@
 import os
 import requests
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
-import time
 
 fruits = ['apple', 'banana', 'corn', 'kiwi', 'lemon', 'orange', 'strawberry']
+prompts = [
+    "Ripe", 
+    "Unripe", 
+    "On a tree", 
+    "In a basket", 
+    "In a bowl", 
+    "Slices", 
+    "Close-up", 
+    "With leaves", 
+    "With stem", 
+    "With seeds", 
+    "On a plate", 
+    "In a smoothie", 
+    "In a salad", 
+    "In a pie", 
+    "In a cocktail", 
+    "On a sandwich", 
+    "In a dessert", 
+    "In a fruit salad", 
+    "With other fruits", 
+    "In different colors varieties"
+]
 
 for fruit in fruits:
-    folder_name = 'webscraping/fruits/' + fruit.capitalize()
+    folder_name = 'webscraping/fruits/' + fruit
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
-    count = 0
-    start_index = 1
-    while count < 250:
-        headers = {'User-Agent': UserAgent().random}
-        url = f'https://www.google.com/search?q={fruit}+fruit&tbm=isch&ijn={start_index}'
-        response = requests.get(url, headers=headers)
+    for prompt in prompts:
+        query = f'{prompt} {fruit}'
+        url = f'https://www.google.com/search?q={query}&source=lnms&tbm=isch'
+        response = requests.get(url)
         soup = BeautifulSoup(response.content, 'html.parser')
         images = soup.find_all('img')
         for i, image in enumerate(images):
-            if count >= 250:
-                break
-            image_url = image['src']
-            filename = os.path.join(folder_name, f'{fruit}_{count}.jpg')
-            with open(filename, 'wb') as f:
-                if 'http' in image_url:
-                    f.write(requests.get(image_url, headers=headers).content)
-            count += 1
-        start_index += 1
-        # wait for 10 milliseconds
-        time.sleep(0.01)
+            if i < 20:  # Download only the first 10 images for each query
+                image_url = image['src']
+                filename = os.path.join(folder_name, f'{query}_{i}.jpg')
+                # Download the image is not a gif
+                if not image_url.endswith('.gif'):
+                    with open(filename, 'wb') as f:
+                        f.write(requests.get(image_url).content)
